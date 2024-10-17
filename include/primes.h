b0c555261c53cf9416df761c6f65b5ba0a16d2f5
Filip Forsström <83241499+filipforsstrom@users.noreply.github.com>
2024-10-17 15:11:29 +0200

    first

---
+++ include/primes.h
@@ -0,0 +1,12 @@
+#ifndef PRIMES_H
+#define PRIMES_H
+
+char *iarr_to_str(char *buf, int *iarr, int ialen);
+
+bool is_prime(int num);
+
+int get_next_prime(int prime);
+
+int *prime_factors(int *buffer, int *factors, int num);
+
+#endif // PRIMES_H
